8fcbb4b6e5d5d50c56706bb986ee56f93cde0b6f
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-09-28 16:34:03 -0300

    Product page construction

--- src/pages/Product/[id].tsx
+++ src/pages/Product/[id].tsx
@@ -1,9 +1,27 @@
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import camisa1 from '../../assets/1.png'
 
 export default function Product() {
 
     const {query} = useRouter()
     return (
-        <h1>Product = {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                <Image  src={camisa1}/>
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, sed similique exercitationem alias ipsum illum enim aliquam nostrum autem reprehenderit perspiciatis, veniam officia magni velit debitis consequatur soluta amet voluptates.</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
